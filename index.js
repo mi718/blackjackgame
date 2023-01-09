c690915012ff553dea30ba348478eb96a1387ccc
micael <micael.staeubli@gmail.com>
2023-01-09 13:25:20 +0100

    feat: cards / sum display onn page

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let firstCard = 10 
-let secondCard = 7
+let firstCard = 11
+let secondCard = 10
 let sum = firstCard + secondCard
+console.log(sum)
+
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+
+let messageEl = document.getElementById("message-El")
+let cardEl = document.getElementById("card-El")
+let sumEl = document.getElementById("sum-El")
 
 function startGame(){
 
@@ -13,6 +19,9 @@ function startGame(){
 // if less than or equal to 20   -> "Do you want to draw a new card?"
 // else if exactly 21 -> "Blackjack"
 // else     -> "You're out of the game"
+
+cardEl.textContent = "Cards: " + firstCard + " " + secondCard 
+sumEl.textContent = "Sum: " + sum
 
 if(sum <= 20){
  message = "Do you want to draw a new card?"
@@ -24,7 +33,7 @@ if(sum <= 20){
  isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
 
 // add a has Blackjack variable
 console.log(hasBlackjack)
