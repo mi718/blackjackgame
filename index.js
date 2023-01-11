67c27e408451a7da00a4ce14f172b962a623d7fb
micael <micael.staeubli@gmail.com>
2023-01-11 11:40:37 +0100

    feat: Dealer section implement / card-imges upload

--- index.js
+++ index.js
@@ -15,15 +15,82 @@ function newCard(){
  document.getElementById("newCard").style.display = 'none'  
 }
 
+const card_img = [
+     {value:1 , img:'../deckOfCards/ace_of_clubs.png'},
+     {value:1  , img:'../deckOfCards/ace_of_diamonds.png'},
+     {value:1  , img:'../deckOfCards/ace_of_hearts.png'},
+     {value:1  , img:'../deckOfCards/ace_of_spades.png'},
+     {value:2  , img:'../deckOfCards/2_of_clubs.png'},
+     {value:2  , img:'../deckOfCards/2_of_diamonds.png'},
+     {value:2  , img:'../deckOfCards/2_of_hearts.png'},
+     {value:2  , img:'../deckOfCards/2_of_spades.png'},
+     {value:3  , img:'../deckOfCards/3_of_clubs.png'},
+     {value:3  , img:'../deckOfCards/3_of_diamonds.png'},
+     {value:3  , img:'../deckOfCards/3_of_hearts.png'},
+     {value:3  , img:'../deckOfCards/3_of_spades.png'},
+     {value:4  , img:'../deckOfCards/4_of_clubs.png'},
+     {value:4  , img:'../deckOfCards/4_of_diamonds.png'},
+     {value:4  , img:'../deckOfCards/4_of_hearts.png'},
+     {value:4  , img:'../deckOfCards/4_of_spades.png'},
+     {value:5  , img:'../deckOfCards/5_of_clubs.png'},
+     {value:5  , img:'../deckOfCards/5_of_diamonds.png'},
+     {value:5  , img:'../deckOfCards/5_of_hearts.png'},
+     {value:5  , img:'../deckOfCards/5_of_spades.png'},
+     {value:6  , img:'../deckOfCards/6_of_clubs.png'},
+     {value:6  , img:'../deckOfCards/6_of_diamonds.png'},
+     {value:6  , img:'../deckOfCards/6_of_hearts.png'},
+     {value:6  , img:'../deckOfCards/6_of_spades.png'},
+     {value:7  , img:'../deckOfCards/7_of_clubs.png'},
+     {value:7  , img:'../deckOfCards/7_of_diamonds.png'},
+     {value:7  , img:'../deckOfCards/7_of_hearts.png'},
+     {value:7  , img:'../deckOfCards/7_of_spades.png'},
+     {value:8  , img:'../deckOfCards/8_of_clubs.png'},
+     {value:8  , img:'../deckOfCards/8_of_diamonds.png'},
+     {value:8  , img:'../deckOfCards/8_of_hearts.png'},
+     {value:8  , img:'../deckOfCards/8_of_spades.png'},
+     {value:9  , img:'../deckOfCards/9_of_clubs.png'},
+     {value:9  , img:'../deckOfCards/9_of_diamonds.png'},
+     {value:9  , img:'../deckOfCards/9_of_hearts.png'},
+     {value:9  , img:'../deckOfCards/9_of_spades.png'},
+     {value:10 , img:'../deckOfCards/10_of_clubs.png' },
+     {value:10 , img:'../deckOfCards/10_of_diamonds.png' },
+     {value:10 , img:'../deckOfCards/10_of_hearts.png' },
+     {value:10 , img:'../deckOfCards/10_of_spades.png' },
+     {value:10 , img:'../deckOfCards/jack_of_clubs.png' },
+     {value:10 , img:'../deckOfCards/jack_of_diamonds.png' },
+     {value:10 , img:'../deckOfCards/jack_of_hearts.png' },
+     {value:10 , img:'../deckOfCards/jack_of_spades.png' },
+     {value:10 , img:'../deckOfCards/queen_of_clubs.png' },
+     {value:10 , img:'../deckOfCards/queen_of_diamonds.png' },
+     {value:10 , img:'../deckOfCards/queen_of_hearts.png' },
+     {value:10 , img:'../deckOfCards/queen_of_spades.png' },
+     {value:10 , img:'../deckOfCards/king_of_clubs.png' },
+     {value:10 , img:'../deckOfCards/king_of_diamonds.png' },
+     {value:10 , img:'../deckOfCards/king_of_hearts.png' },
+     {value:10 , img:'../deckOfCards/king_of_spades.png' },
+     {value:11 , img:'../deckOfCards/aces_of_clubs.png' },
+     {value:11 , img:'../deckOfCards/aces_of_diamonds.png' },
+     {value:11 , img:'../deckOfCards/aces_of_hearts.png' },
+     {value:11 , img:'../deckOfCards/aces_of_spades.png' }
+]
+
+console.log(card_img[Math.floor(Math.random()*52)])
+
+function randomcard_img(){
+     let selectCard = card_img[Math.floor(Math.random()*52)]
+     let firstDealerCard = document.getElementById("")
+}
+
 function startGame(){
 document.getElementById("start-btn").style.display = 'none'
 document.getElementById("gameOptions").style.display = 'block'
+document.getElementById("dealerCard").style.display = 'block'
 cardEl.textContent = "Cards: " + cards 
-sumEl.textContent = "Sum: " + sum
+sumEl.textContent = "Sum: " + sum 
 
 if(sum <= 20){
  message = "Do you want to draw a new card?"
- document.getElementById("extra-btn").style.display = 'block'
+ document.getElementById("extra-btn").style.display = 'block' 
 }else if(sum === 21){
  message = "You've got Blackjack"
  document.getElementById("extra-btn").style.display = 'none'
